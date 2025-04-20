179442a4bd1212e1207a04d609dd9c193d10233f
Igor <apofiosis2@gmail.com                                                                                git config --global user.name Igor>
2025-04-20 13:21:17 +0300

    new file

--- lesson/test.py
+++ lesson/test.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_argument('--headless') # безголовый реж
-options.add_argument("--window-size=1920x1080") # Запуск браузера с заданным разрешением
+#options.add_argument("--window-size=1920x1080") # Запуск браузера с заданным разрешением
 options.add_argument("--disable-blink-features=AutomationControlled") # Отключение средства автоматизации т.е. браузером управляет человек
 # подмена user-agenta
 options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
