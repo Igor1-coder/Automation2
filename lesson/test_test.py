c775291dcca09342f9e60bd4d2630a367d0011b5
Igor <apofiosis2@gmail.com                                                                                git config --global user.name Igor>
2025-04-27 19:16:54 +0300

    pytes

--- lesson/test_test.py
+++ lesson/test_test.py
@@ -14,7 +14,6 @@ def test_demoqa_form():
     options.add_argument("--disable-blink-features=AutomationControlled") # Отключение средства автоматизации т.е. браузером управляет человек
     # подмена user-agenta
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
-
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     wait =WebDriverWait(driver,5,poll_frequency=1)
