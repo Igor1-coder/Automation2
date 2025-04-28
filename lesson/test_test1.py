9080002e62f7f25f2ebb4e1baa477a81e8337973
Igor <apofiosis2@gmail.com                                                                                git config --global user.name Igor>
2025-04-28 16:50:19 +0300

    pytesss

--- lesson/test_test1.py
+++ lesson/test_test1.py
@@ -22,51 +22,51 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 wait =WebDriverWait(driver,5,poll_frequency=1)
 
-driver.get("https://demoqa.com/text-box")
+    driver.get("https://demoqa.com/text-box")
 
-time.sleep(3)
+    time.sleep(3)
 
-Full_name = driver.find_element("xpath","//input[@placeholder='Full Name']")
+    Full_name = driver.find_element("xpath","//input[@placeholder='Full Name']")
 
-assert Full_name.get_attribute("value")==""
+    assert Full_name.get_attribute("value")==""
 
-Full_name.send_keys("Igor")
-value = Full_name.get_attribute("value") # создаём переменную value и получаем значение из input-поля
-assert "Igor" in value # Проверяем что "Igor" есть в значение value
-print(value) # выводим значения
+    Full_name.send_keys("Igor")
+    value = Full_name.get_attribute("value") # создаём переменную value и получаем значение из input-поля
+    assert "Igor" in value # Проверяем что "Igor" есть в значение value
+    print(value) # выводим значения
 
-time.sleep(3)
+    time.sleep(3)
 
-email_name = driver.find_element("xpath","//input[@placeholder='name@example.com']")
+    email_name = driver.find_element("xpath","//input[@placeholder='name@example.com']")
 
-assert email_name.get_attribute("value")==""
+    assert email_name.get_attribute("value")==""
 
-email_name.send_keys("qwer@.com")
+    email_name.send_keys("qwer@.com")
 
-time.sleep(3)
+    time.sleep(3)
 
-Current_Address = driver.find_element("xpath","//textarea[@placeholder='Current Address']")
+    Current_Address = driver.find_element("xpath","//textarea[@placeholder='Current Address']")
 
-Current_Address.send_keys("Street krolevec 62")
-value1 = Current_Address.get_attribute("value") # Получаем значение из input-поля
-assert "Street krolevec 62"== value1
-print(value1)
-time.sleep(3)
+    Current_Address.send_keys("Street krolevec 62")
+    value1 = Current_Address.get_attribute("value") # Получаем значение из input-поля
+    assert "Street krolevec 62"== value1
+    print(value1)
+    time.sleep(3)
 
-Permanent_Address = driver.find_element("xpath","//textarea[@id='permanentAddress']")
+    Permanent_Address = driver.find_element("xpath","//textarea[@id='permanentAddress']")
 
-Permanent_Address.send_keys("adrrees 2")
+    Permanent_Address.send_keys("adrrees 2")
 
-time.sleep(3)
+    time.sleep(3)
 
-driver.get("https://the-internet.herokuapp.com/status_codes")
+    driver.get("https://the-internet.herokuapp.com/status_codes")
 
-Code_200 = driver.find_element("xpath","//a[@href='status_codes/200'] ")
-time.sleep(3)
-Code_200.click()
+    Code_200 = driver.find_element("xpath","//a[@href='status_codes/200'] ")
+    time.sleep(3)
+    Code_200.click()
 
-time.sleep(3)
+    time.sleep(3)
 
-driver.back()
+    driver.back()
 
-driver.quit()
+    driver.quit()
